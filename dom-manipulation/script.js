f5276d59dc6b367e5cc166c75f84b078d49e1599
UrsulaKhanguha <140917812+UrsulaKhanguha@users.noreply.github.com>
2025-02-07 09:49:32 +0300

    Update script.js

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -87,7 +87,16 @@ function exportQuotes(){
 }
 // Allan Added This Above Piece //
 
-
+function importFromJsonFile(event) {
+    const fileReader = new FileReader();
+    fileReader.onload = function(event) {
+      const importedQuotes = JSON.parse(event.target.result);
+      quotes.push(...importedQuotes);
+      saveQuotes();
+      alert('Quotes imported successfully!');
+    };
+    fileReader.readAsText(event.target.files[0]);
+  }
 
   // document.addEventListener("DOMContentLoaded", importFromJsonFile());
 
